cc8ecead0a8de846251ec4f27f683e51e6809fc4
Sam Petherbridge <Sam@Peth.me>
2016-07-10 11:13:27 +1000

    Changed most references from OS X to macOS in line with apples new name

--- macos.sh
+++ macos.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# ~/osx.sh — Originally from https://mths.be/osx
+# ~/macos.sh — Originally from https://mths.be/osx
 
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
+# Keep-alive: update existing `sudo` time stamp until `macos.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
